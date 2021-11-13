cadb1d5cecb79b771ad059f90e1247e24e1fc6f1
Mümin <zehir.m.54@gmail.com>
2021-11-13 19:21:37 +0300

    update ve delete istekleri oluşturuldu

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,12 +1,21 @@
 import Post from "../models/posts.js";
 
-// Burada db'ye çağrı isteklerinde bulunacağız.
 export const getPosts = async (req, res) => {
   try {
-    //Db'den dönen postlar
-    const posts = await Post.find(); // Db'de ne kadar post varsa onları bul getir.
-    //Geriye dönen cevap
-    res.status(200).json(posts); // Geriye json formatın res döndürüyoruz.
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+
+export const getSinglePost = async (req, res) => {
+  try {
+    const { id: _id } = req.params;
+    const post = await Post.findById(_id);
+    res.status(200).json(post);
   } catch (error) {
     res.status(404).json({
       message: error.message,
@@ -16,11 +25,34 @@ export const getPosts = async (req, res) => {
 
 export const createPost = async (req, res) => {
   const newPost = new Post(req.body);
-  // Yeni Post İşlem Akışı:
-  //  5- Bu adımda newPost ismiyle yeni bir post modeli oluşturuluyor.
   try {
-    await newPost.save(); // Ve bu satır ile veritabanına kaydediliyor.
-    res.status(201).json(newPost); // Ve bu kayıt başarılı olursa bunu res ile fronta geri döndürüyoruz.
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({
+      message: error.message,
+    });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(_id, post, { new: true });
+    res.json(updatedPost);
+  } catch (error) {
+    res.status(409).json({
+      message: error.message,
+    });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  const { id: _id } = req.params;
+  try {
+    const deletedPost = await Post.findByIdAndRemove(_id);
+    res.json(deletedPost);
   } catch (error) {
     res.status(409).json({
       message: error.message,
--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { getPosts, createPost } from "../controllers/posts.js";
+import {
+  getPosts,
+  createPost,
+  getSinglePost,
+  deletePost,
+  updatePost,
+} from "../controllers/posts.js";
 
 const router = express.Router();
 router.get("/", getPosts);
-router.post("/", createPost); // Post isimli bir req geldiği zaman işlemi createPost'a devret. Yapılması gereken işlemi o yapsın.
-// Yeni Post İşlem Akışı:
-//  4- Bu adımda bu post buraya düşüyor ve createPost router'ıda bunu createPost controller'ına gönderiyor.
+router.get("/:id", getSinglePost);
+router.post("/", createPost);
+router.patch("/:id", updatePost);
+router.delete("/:id", deletePost);
 
 export default router;
